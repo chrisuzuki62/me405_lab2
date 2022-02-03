f1147316340040900b41c71cd9fcf287ab9f7a5a
chrisor8 <97257128+chrisor8@users.noreply.github.com>
2022-02-03 00:18:40 -0800

    Update 2 comments pc_com.py

--- src/pc_com.py
+++ src/pc_com.py
@@ -15,20 +15,6 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-def is_number(string):
-    """!
-    @brief   Identifies if the csv value can be converted into a float
-    @param   string    The entire csv value to be tested
-    """
-    try:
-        ## Try converting the string value to a float value
-        float(string)
-        ## Return boolean true if possible
-        return True
-    except ValueError:
-        ## Return boolean false if not possible
-        return False
-
 ## Set the communication channel and serial data rate for the serial port
 with serial.Serial('COM3', 115200) as s_port:
 
@@ -36,29 +22,29 @@ with serial.Serial('COM3', 115200) as s_port:
         ## Set input as any text value and enter to proceed
         start_step = input("Type any text + Enter to run a step response ")
         try:
-            ## Convert string value to a float
+            # Convert string value to a float
             float(start_step)
-            ## Revert value to string to encode and add return
+            # Revert value to string to encode and add return
             s_port.write(str(start_step).encode() + b'\r')
             break
         except ValueError:
-            ## Exception for an input that is not numerical
+            # Exception for an input that is not numerical
             print('Invalid Value')
 
-    ## Boolean variable for 
+    ## Boolean variable describing whether all of the data has been read
     bool = True
     ## Initialize list for data to be collected
     data = []
     
-    ## Checks for when the data has finished and ends the loop
+    # Check for when the data has finished and ends the loop
     while bool:
         buffer = s_port.readline()
-        ## At 'DATA' the data has finished
+        # At 'DATA' the data has finished
         if buffer == b'DATA\r\n':
             bool = False
             break
         else:
-            ## Appends data to the 'data' list
+            # Appends data to the 'data' list
             data.append(buffer.decode())
             
 ## Initialize lists for data to be plotted        
@@ -68,27 +54,27 @@ y = []
 ## Object for number of runs to complete all data points
 runs = 0
 
-## Loops until data is finished
+# Loops until data is finished
 for idx in data:
     if runs != 0:
-        ## Deletes irrelevant characters and splits csv data
+        # Deletes irrelevant characters and splits csv data
         p = idx[0:-3].split(',')
-        ## Eliminates empty data and appends to each corresponding list based on position
+        # Eliminates empty data and appends to each corresponding list based on position
         if p[0] != "" and p[1] != "":
             x.append(p[0])
             y.append(p[1])
-    ## Adds to run for each iteration of the loop
+    # Adds to run for each iteration of the loop
     runs += 1
 
-## Plot the resulting data
-## Set the x and y tick ranges for the axes
+# Plot the resulting data
+# Set the x and y tick ranges for the axes
 plt.xticks(range(int(min(x)),int(max(x)), 10))
 plt.yticks(range(int(min(y)),int(max(y)), 10))
-## Plot the data
+# Plot the data
 plt.plot(x,y)
-## Axes labels for x and y axes
+# Axes labels for x and y axes
 plt.xlabel('Time (Sec)')
 plt.ylabel('Encoder Value (Ticks)')
-## Show plot on Thonny
+# Show plot on Thonny
 plt.show()
 
