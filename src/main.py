7f340b5cb472caf414069bf375af86fff65d0095
chrisuzuki62 <chrisuzuki62@gmail.com>
2022-02-02 23:59:04 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 """!
 @file       main.py
-@brief      
-@details    
+@brief    Runs the main program and creates all objects to rotate the motor the to desired angle  
+@details  This program is to be installed and ran on the MicroPython board. It creates the class
+          objects motor, controller, and encoder. The program runs a loop until the user types a
+          value into the serial port starting the step response test. The motor will run to the desired
+          angle rely on the encoder and controller feedback to control the motor output.
 
 @author Damond Li
 @author Chris Or
@@ -17,7 +20,8 @@ import utime
 import struct
 import time
 
-# Create objects
+
+##Create objects
 mtr1 = motor.Motor(1)
 enc1 = encoder.Encoder(1)
 ctr = controller.Controller()
@@ -26,16 +30,19 @@ while True:
     try:
         # Enable motor
         mtr1.enable()
+        
+        # Test code to be used later
         #tempgain = b'0.35\r'
         #input_gain = float(input().decode()[0:-1])
         #print(type(input_gain))
         #ctr.set_gain(tempgain.decode()[0:-1])
-        
         #input_position = int(input().decode()[0:-1])
+        
         input()
         ctr.set_gain(0.01)
         ctr.set_position(20000)
         
+        # Create empty variables
         ctr.positional_data = []
         time_array = []
         position_array = []
