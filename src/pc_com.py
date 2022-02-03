3363ec97c2c164462d7d125726a08623613c131c
chrisor8 <97257128+chrisor8@users.noreply.github.com>
2022-02-02 23:57:14 -0800

    Comments for pc_com.py

--- src/pc_com.py
+++ src/pc_com.py
@@ -1,16 +1,14 @@
 '''!
-    @file pc_com.py
-    @brief A driver for communicating data from the encoders
-    @details This file creates an encoder calss that contains 3 methods that can
-             be used in other files to get the change of the position of an
-             encoder over time. It contains a construcotr method to instantiate each
-             class object. A  get position method that returns the position of the encoder. 
-             A zero poistion method that zeroes the position of the encoder to a specifed
-             value. 
-    @author Damond Li
-    @author Chris Or
-    @author Chris Suzuki
-    @date 1/25/22
+@file     pc_com.py
+@brief    A program that runs a step response prompted by the PC and plots resulting data
+@details  This is a program for the PC which will run step response tests by sending
+          characters through the USB serial port to the Nucleo board, reading the resulting data,
+          and plotting the step response with labeled axes. This program allows us to efficiently
+          test various gain, Kp, values on a step response run through a prompt from the PC user. 
+@author Damond Li
+@author Chris Or
+@author Chris Suzuki
+@date 2/2/22
 '''
 import serial
 import time
@@ -23,70 +21,74 @@ def is_number(string):
     @param   string    The entire csv value to be tested
     """
     try:
-        # Try converting the string value to a float value
+        ## Try converting the string value to a float value
         float(string)
-        # Return boolean true if possible
+        ## Return boolean true if possible
         return True
     except ValueError:
-        # Return boolean false if not possible
+        ## Return boolean false if not possible
         return False
 
-
+## Set the communication channel and serial data rate for the serial port
 with serial.Serial('COM3', 115200) as s_port:
 
-
     while True:
-        gain = input("Please input the desired gain: ")
+        ## Set input as any text value and enter to proceed
+        start_step = input("Type any text + Enter to run a step response ")
         try:
-            float(gain)
-            s_port.write(str(gain).encode() + b'\r')
+            ## Convert string value to a float
+            float(start_step)
+            ## Revert value to string to encode and add return
+            s_port.write(str(start_step).encode() + b'\r')
             break
         except ValueError:
+            ## Exception for an input that is not numerical
             print('Invalid Value')
-    
-#     while True:    
-#         position = input("Please input the desired position: ")
-#         try:
-#             int(position)
-#             s_port.write(str(position).encode() + b'\r')
-#             break
-#         except ValueError:
-#             print('Invalid Value')        
-    
+
+    ## Boolean variable for 
     bool = True
+    ## Initialize list for data to be collected
     data = []
     
+    ## Checks for when the data has finished and ends the loop
     while bool:
-        temp = s_port.readline()
-        if temp == b'DATA\r\n':
+        buffer = s_port.readline()
+        ## At 'DATA' the data has finished
+        if buffer == b'DATA\r\n':
             bool = False
             break
         else:
-            data.append(temp.decode())
+            ## Appends data to the 'data' list
+            data.append(buffer.decode())
             
-
-
+## Initialize lists for data to be plotted        
 x = []
 y = []
 
-
+## Object for number of runs to complete all data points
 runs = 0
 
+## Loops until data is finished
 for idx in data:
     if runs != 0:
+        ## Deletes irrelevant characters and splits csv data
         p = idx[0:-3].split(',')
+        ## Eliminates empty data and appends to each corresponding list based on position
         if p[0] != "" and p[1] != "":
             x.append(p[0])
             y.append(p[1])
+    ## Adds to run for each iteration of the loop
     runs += 1
 
-#plt.axis([min(x), max(x), min(y), max(y)])
-# plt.yticks(np.arange(int(min(y)), int(max(y)) + 1, step = 5, dtype=int))
-# plt.xticks(np.arange(int(min(x)), int(max(x)) + 1, step = 10, dtype=int))
+## Plot the resulting data
+## Set the x and y tick ranges for the axes
 plt.xticks(range(int(min(x)),int(max(x)), 10))
 plt.yticks(range(int(min(y)),int(max(y)), 10))
+## Plot the data
 plt.plot(x,y)
+## Axes labels for x and y axes
 plt.xlabel('Time (Sec)')
 plt.ylabel('Encoder Value (Ticks)')
+## Show plot on Thonny
 plt.show()
 
